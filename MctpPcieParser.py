c7ab37fcf8fa331783d49fe16123771aa32973a2
Michał <mduzin@o2.pl>
2017-08-02 20:27:59 +0200

    Add Mctp PCie VDM parser module

---
+++ MctpPcieParser.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  2 20:06:18 2017
+
+@author: M
+"""
+
+#MCTP PCIe VDM frame parser
+
+#import MctpParser
+
+
+def ParseMctpPcieFrame(PcieFrame):
+    #check basic frame size
+    #parse PCIe VDM Header
+    #parse PCIe VDM Data (parse MCTP Frame)
+    return
+
+#----Script Start----
+if __name__ == "__main__":
+
